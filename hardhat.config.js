2153d8311489ffe513e25201e4556afe4ba65d09
GautamR24 <rawatgautam24@protonmail.com>
2022-11-24 21:55:42 +0530

    added the etherscan API

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require('dotenv').config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.15",
+  networks: {
+    goerli: {
+      url: process.env.REACT_APP_ALCHEMY_RPC,
+      accounts: [process.env.REACT_APP_PRIVATE_KEY],
+    }
+
+  },
+  etherscan: {
+    apiKey: process.env.REACT_API_ETHER
+
+  },
 };
